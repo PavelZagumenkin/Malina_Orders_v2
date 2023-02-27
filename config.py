3fb7c5cdc1f9772e28f27ff72730d76ceaad5616
Pavel Zagumenkin <pavelzagumenkin@gmail.com>
2023-02-27 17:11:05 +0400

    Учим новую БД

---
+++ config.py
@@ -0,0 +1,5 @@
+host = "127.0.0.1"
+user = "postgres"
+password = "88888889"
+db_name = "malina64"
+port = 5432
--- malina64.py
+++ malina64.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: UTF8 -*-
 # !/usr/bin/env python
 import psycopg2
 from config import host, user, password, db_name
@@ -11,14 +11,12 @@ try:
         password=password,
         database=db_name
     )
-
     # Создаем курсор
     with connection.cursor() as cursor:
         cursor.execute(
-            "SELECT version();"
+            "SELECT * FROM konditerskie;"
         )
-        print(f"Версия сервера: {cursor.fetchone()}")
-
+        print(f"Строка: {cursor.fetchone()}")
 except Exception as _ex:
     print("[INFO] Ошибка работы с БД", _ex)
 finally:
