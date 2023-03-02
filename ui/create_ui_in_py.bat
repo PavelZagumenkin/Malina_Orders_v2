f8accc82dcf47fb6756be11f50b67e85f0f44aa6
Pavel Zagumenkin <pavelzagumenkin@gmail.com>
2023-03-02 10:14:34 +0400

    Добавил панель управления для администратора

--- ui/create_ui_in_py.bat
+++ ui/create_ui_in_py.bat
@@ -1,6 +1,6 @@
-pyuic6 -x authorization.ui -o authorization.py
-pyuic6 -x sections.ui -o sections.py
-@REM pyuic6 -x bakery.ui -o bakery.py
+@REM pyuic6 -x authorization.ui -o authorization.py
+@REM pyuic6 -x sections.ui -o sections.py
+pyuic6 -x control.ui -o control.py
 @REM pyuic6 -x pie.ui -o pie.py
 @REM pyuic6 -x cake.ui -o cake.py
 @REM pyuic6 -x filling.ui -o filling.py
